29cd89762c983bd8b3ca451c2c023d9255d4c631
nchronas <nchronas@gmail.com>
2018-04-20 15:56:28 +0200

    HLDCL serial deframe skeleton

--- src/main/java/org/xtce/test/DelfiC3.java
+++ src/main/java/org/xtce/test/DelfiC3.java
@@ -3,6 +3,7 @@ package org.xtce.test;
 import java.io.File;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Arrays;
 import org.xtce.toolkit.XTCEContainerContentEntry;
 import org.xtce.toolkit.XTCEContainerContentModel;
 import org.xtce.toolkit.XTCEContainerEntryValue;
@@ -10,14 +11,19 @@ import org.xtce.toolkit.XTCEDatabase;
 import org.xtce.toolkit.XTCEDatabaseException;
 import org.xtce.toolkit.XTCETMStream;
 
+import com.fazecast.jSerialComm.*;
+
 /**
  * Example Delfi-C3 XTCE telemetry extractor
  *
  */
-public class DelfiC3 
+public class DelfiC3
 {
 
-    public static void main(String[] args) 
+  static byte[] temp_buffer = new byte[100];
+  static int temp_counter = 0;
+
+    public static void main(String[] args)
     {
 
         byte[] hk = new byte[]{(byte) 0xA8, (byte) 0x98, (byte) 0x9A, (byte) 0x40, (byte) 0x40, (byte) 0x40,
@@ -65,12 +71,12 @@ public class DelfiC3
 
         String file = "Delfi-C3.xml";
 
-        try 
+        try
         {
             System.out.println("Loading " + file + " database");
 
             XTCEDatabase db_ = new XTCEDatabase(new File(file), true, false, true);
-            
+
             List<String> warnings = db_.getDocumentWarnings();
             Iterator<String> it = warnings.iterator();
             while(it.hasNext())
@@ -83,18 +89,108 @@ public class DelfiC3
             processFrame(stream, p);
             processFrame(stream, hk);
 
-        } catch (XTCEDatabaseException ex) 
+            System.out.println("Yo, how are you?");
+
+        } catch (XTCEDatabaseException ex)
         {
             ex.printStackTrace();
         } catch (Exception ex) {
             ex.printStackTrace();
         }
+
+
+        while(true) {
+          SerialPort comPort = SerialPort.getCommPort("/dev/ttyACM0");
+          comPort.openPort();
+          comPort.addDataListener(new SerialPortDataListener() {
+            @Override
+            public int getListeningEvents() { return SerialPort.LISTENING_EVENT_DATA_AVAILABLE; }
+            @Override
+              public void serialEvent(SerialPortEvent event)
+              {
+
+
+                if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE)
+                  return;
+                while(comPort.bytesAvailable() > 0) {
+
+                  byte[] newData = new byte[1];
+                  int numRead = comPort.readBytes(newData, 1);
+
+                  if(newData[0]== 0x7e) {
+                    temp_buffer[0] = 0x7e;
+                    temp_counter = 1;
+                  } else if(newData[0]== 0x7c) {
+
+                    temp_buffer[0] = 0x7e;
+
+                    byte[] passingBy = Arrays.copyOf(temp_buffer, temp_counter);
+                    temp_counter = 0;
+                    System.out.println("Read " + passingBy.length + " bytes." + new String(passingBy, 0));
+
+                    hdlc_deframe(passingBy);
+
+
+                  } else if(temp_counter > 0) {
+                    temp_buffer[temp_counter++] = newData[0];
+                  }
+
+                }
+
+              }
+            });
+        }
     }
-    
-    static void processFrame(XTCETMStream stream, byte[] data) throws XTCEDatabaseException, Exception 
+
+
+    static byte[] hdlc_deframe(byte[] framed) {
+
+      int cnt = 0;
+      byte[] res = new byte[100];
+
+      for(int idx = 0; idx < framed.length; idx++) {
+
+          if(idx == 0 && framed[idx] != 0x7E) {  //HLDLC_START_FLAG
+              System.out.println("Error in HLDC deframe");
+              //throw exeption
+          }
+
+          if(framed[idx] == 0x7C) { //HLDLC_STOP_FLAG
+              byte[] temp = new byte[cnt];
+              for(int i = 0; i <= cnt; i++) {
+                temp[i] = res[i];
+              }
+              System.out.println("HLDC deframe complete " + temp.length + " bytes." + new String(temp, 0));
+              return temp;
+
+          } else if(framed[idx] == 0x7D) { //HLDLC_CONTROL_FLAG
+
+              if(framed[idx] == 0x5E) {
+                res[cnt++] = 0x7E;
+              } else if(framed[idx] == 0x5D) {
+                res[cnt++] = 0x7D;
+              } else if(framed[idx] == 0x5C) {
+                res[cnt++] = 0x7C;
+              } else {
+                System.out.println("Error in HLDC deframe");
+                //throw exeption
+              }
+              idx++;
+          } else {
+              res[cnt++] = framed[idx];
+          }
+      }
+
+      System.out.println("Error in HLDC deframe");
+      //throw exeption
+      return res;
+    }
+
+
+    static void processFrame(XTCETMStream stream, byte[] data) throws XTCEDatabaseException, Exception
     {
         XTCEContainerContentModel model = stream.processStream( data );
- 
+
         List<XTCEContainerContentEntry> entries = model.getContentList();
 
         for (XTCEContainerContentEntry entry : entries) {
@@ -102,10 +198,10 @@ public class DelfiC3
 
             XTCEContainerEntryValue val = entry.getValue();
 
-            if (val == null) 
+            if (val == null)
             {
                 System.out.println();
-            } else 
+            } else
             {
                 System.out.println(": " + val.getCalibratedValue() + " "
                         + entry.getParameter().getUnits() + " ("
